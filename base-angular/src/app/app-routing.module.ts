0648e807efcf2ce02623ab4a242135b6cef9d121
thuannd <thuansmile2479@gmail.com>
2023-05-30 20:19:18 +0700

    Header, Footer

--- base-angular/src/app/app-routing.module.ts
+++ base-angular/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutClientComponent,
+    children: [
+      { path: '', component: HomeComponent }, 
+    ]
+  },
+  {
+    path: 'admin',
+    component: LayoutAdminComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: ProductsComponent }
+    ],
+  },
+
+  { path: '**', component: NotfoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
